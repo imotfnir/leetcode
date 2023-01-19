01117053aed2265edbdc5352b7abb92c766f1fff
imotfnir <imotfnir@gmail.com>
2023-01-19 10:28:02 +0800

    feat: reverse linked list

--- linked_list.c
+++ linked_list.c
@@ -4,24 +4,25 @@
 
 typedef struct Node Node;
 
-void append_node(Node **list, int num);
+void append_node(Node **head, int value);
 void print_list(Node *list);
+void reverse_list(Node **head);
 
 struct Node {
     int data;
     Node *next;
 };
 
-void append_node(Node **list, int num) {
+void append_node(Node **head, int value) {
     Node *tail = (Node *)malloc(sizeof(Node));
-    Node *current = *list;
+    Node *current = *head;
 
-    tail->data = num;
+    tail->data = value;
     tail->next = NULL;
 
-    if(list == NULL) {
+    if(head == NULL) {
         printf("a");
-        *list = tail;
+        *head = tail;
     }
 
     while(current->next != NULL) {
@@ -44,17 +45,36 @@ void print_list(Node *list) {
     return;
 }
 
+void reverse_list(Node **head) {
+    Node *prev = NULL;
+    Node *next = NULL;
+    Node *current = *head;
+
+    while(current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    *head = prev;
+    return;
+}
+
 int main() {
     Node *list_a = NULL;
 
+    list_a = &(Node){ 1, list_a };
     list_a = &(Node){ 2, list_a };
     list_a = &(Node){ 3, list_a };
-    list_a = &(Node){ 4, list_a };
 
-    append_node(&list_a, 1);
-    append_node(&list_a, 2);
-    append_node(&list_a, 3);
+    append_node(&list_a, 4);
+    append_node(&list_a, 5);
+    append_node(&list_a, 6);
+    print_list(list_a);
+    printf("\n");
 
+    reverse_list(&list_a);
     print_list(list_a);
     return 0;
 }
